ed98a4baabc41f4d39711a9a78c07ec7b43a08a2
ashwinipatil1281-dev <ashwinipatil1281@gmail.com>
2026-01-09 23:46:59 +0530

    Update hospital.py

--- hospital.py
+++ hospital.py
@@ -1,22 +1,13 @@
-def hospital(patient, doctor, date):
-    doctors = ["Dr. Smith", "Dr. John", "Dr. Emily"]
 
-    if doctor in doctors:
-        return (
-            "Appointment booked successfully!\n"
-            "Patient Name: " + patient + "\n"
-            "Doctor: " + doctor + "\n"
-            "Date: " + date
-        )
-    else:
-        return "Doctor not available"
+def vehicle_details(patientname, doctorname, time):
+    result = (
+        f"patientname: {patientname}\n"
+        f"doctorname: {doctorname}\n"
+        f"time: {time}
+    )
+    return result
 
 
-# Run program normally
 if __name__ == "__main__":
-    patient = input("Enter patient name: ")
-    doctor = input("Enter doctor name: ")
-    date = input("Enter appointment date: ")
-
-    result = hospital(patient, doctor, date)
-    print(result)
+   
+    print(vehicle_details("raju", "raja", 10/2/2026))
